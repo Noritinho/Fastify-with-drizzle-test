69aa9e5a92f6b4a18071e2775b413d9692af04de
norita <marnori2001@gmail.com>
2024-07-06 18:06:21 -0300

    refactor: change database accounts service

--- src/modules/accounts/routes/accounts.ts
+++ src/modules/accounts/routes/accounts.ts
@@ -1,105 +1,49 @@
 import { FastifyInstance } from 'fastify';
-import { and, eq, sum } from 'drizzle-orm';
 
-import * as schema from '../../../database/schema/schema';
 import { createAccountSchema } from '../dto/create-account.dto';
 import { updateAccountSchema } from '../dto/update-account.dto';
-import { db } from '../../../database/database.config';
+import { AccountsService } from '../services/accounts.service';
 
 export async function accountsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
-    const accounts = await db.query.accounts.findMany();
-    return accounts;
+    return AccountsService.prototype.findAllAccounts();
   });
 
   app.get<{ Params: { id: number } }>('/:id', async (request) => {
     const { id } = request.params;
 
-    const accounts = await db.query.accounts.findMany({
-      where: eq(schema.accounts.id, id),
-    });
-
-    return accounts;
+    return AccountsService.prototype.findOneAccount(id);
   });
 
   app.get<{ Params: { id: number } }>('/users/:id', async (request) => {
     const { id } = request.params;
 
-    const accounts = await db.query.accounts.findMany({
-      where: eq(schema.accounts.userId, id),
-    });
-
-    return accounts;
+    return AccountsService.prototype.findUserAccounts(id);
   });
 
   app.get<{ Params: { id: number } }>('/users/:id/total', async (request) => {
     const { id } = request.params;
 
-    const accountsReceivable = await db
-      .select({
-        value: sum(schema.accounts.value),
-      })
-      .from(schema.accounts)
-      .where(
-        and(
-          eq(schema.accounts.userId, id),
-          eq(schema.accounts.account_type, 'RECEIVABLE')
-        )
-      );
-
-    const accountsPayable = await db
-      .select({
-        value: sum(schema.accounts.value),
-      })
-      .from(schema.accounts)
-      .where(
-        and(
-          eq(schema.accounts.userId, id),
-          eq(schema.accounts.account_type, 'PAYABLE')
-        )
-      );
-
-    if (!accountsReceivable[0].value && accountsPayable[0].value) {
-      return accountsPayable[0].value;
-    }
-
-    if (accountsReceivable[0].value && !accountsPayable[0].value) {
-      return accountsReceivable[0].value;
-    }
-
-    return accountsReceivable[0].value && accountsPayable[0].value
-      ? parseFloat(accountsReceivable[0].value) -
-          parseFloat(accountsPayable[0].value)
-      : 'Erro';
+    return AccountsService.prototype.findUserTotalAccountValue(id);
   });
 
   app.post<{ Params: { id: number } }>('/users/:id', async (request) => {
     const { id } = request.params;
+    const account = createAccountSchema.parse(request.body);
 
-    const { description, value, account_type } = createAccountSchema.parse(
-      await request.body
-    );
-
-    await db.insert(schema.accounts).values({
-      userId: id,
-      description: description,
-      value: value,
-      account_type: account_type,
-    });
+    return AccountsService.prototype.createAccount(id, account);
   });
 
   app.put<{ Params: { id: number } }>('/id', async (request) => {
     const { id } = request.params;
-    const account = updateAccountSchema.parse(await request.body);
-    await db
-      .update(schema.accounts)
-      .set(account)
-      .where(eq(schema.accounts.id, id));
+    const account = updateAccountSchema.parse(request);
+
+    return AccountsService.prototype.updateAccount(id, account);
   });
 
   app.delete<{ Params: { id: number } }>('/:id', async (request) => {
     const { id } = request.params;
 
-    await db.delete(schema.accounts).where(eq(schema.accounts.id, id));
+    return AccountsService.prototype.deleteAccount(id);
   });
 }
---
+++ src/modules/accounts/services/accounts.service.ts
@@ -0,0 +1,86 @@
+import { and, eq, sum } from 'drizzle-orm';
+
+import { db } from '../../../database/database.config';
+import { accounts } from '../models/accounts-model';
+import { CreateAccountDto } from '../dto/create-account.dto';
+import { UpdateAccountDto } from '../dto/update-account.dto';
+
+export class AccountsService {
+  async findAllAccounts() {
+    const findAllAccounts = await db.query.accounts.findMany();
+    return findAllAccounts;
+  }
+
+  async findOneAccount(id: number) {
+    const findOneAccount = await db.query.accounts.findMany({
+      where: eq(accounts.id, id),
+    });
+
+    return findOneAccount;
+  }
+
+  async findUserAccounts(id: number) {
+    const findUserAccounts = await db.query.accounts.findMany({
+      where: eq(accounts.userId, id),
+    });
+
+    return findUserAccounts;
+  }
+
+  async findUserTotalAccountValue(userId: number) {
+    const accountsReceivable = await db
+      .select({
+        value: sum(accounts.value),
+      })
+      .from(accounts)
+      .where(
+        and(
+          eq(accounts.userId, userId),
+          eq(accounts.account_type, 'RECEIVABLE')
+        )
+      );
+
+    const accountsPayable = await db
+      .select({
+        value: sum(accounts.value),
+      })
+      .from(accounts)
+      .where(
+        and(eq(accounts.userId, userId), eq(accounts.account_type, 'PAYABLE'))
+      );
+
+    if (!accountsReceivable[0].value && accountsPayable[0].value) {
+      return accountsPayable[0].value;
+    }
+
+    if (accountsReceivable[0].value && !accountsPayable[0].value) {
+      return accountsReceivable[0].value;
+    }
+
+    return accountsReceivable[0].value && accountsPayable[0].value
+      ? parseFloat(accountsReceivable[0].value) -
+          parseFloat(accountsPayable[0].value)
+      : 'Erro';
+  }
+
+  async createAccount(userId: number, createAccountDto: CreateAccountDto) {
+    const { description, value, account_type } = createAccountDto;
+
+    await db.insert(accounts).values({
+      userId: userId,
+      description: description,
+      value: value,
+      account_type: account_type,
+    });
+  }
+
+  async updateAccount(userId: number, updateAccountDto: UpdateAccountDto) {
+    const account = updateAccountDto;
+
+    await db.update(accounts).set(account).where(eq(accounts.id, userId));
+  }
+
+  async deleteAccount(id: number) {
+    await db.delete(accounts).where(eq(accounts.id, id));
+  }
+}
